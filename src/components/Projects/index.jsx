a3dbd9bf631dc7f4d64c2dd583ee4d9dfc70107c
worldofwandag <jwandag@gmail.com>
2025-05-05 23:14:58 -0700

    initialize git repo

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from "react"
+import "./style.css"
+import BackgroundLines from "../BackgroundLines"
+import WorkCard from "../WorkCard"
+import ScrambleText from "../ScrambleText"
+import ParaWriting from "../ParaWriting"
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer"
+
+import work1 from "../../assets/Images/summarist-mockup.png"
+import work2 from "../../assets/Images/skinstric-mockup.png"
+import work3 from "../../assets/Images/library-mockup.png"
+
+export default function Projects() {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+  const [hasAnimated, setHasAnimated] = useState(false)
+
+  const handleComplete = () => {
+    setHasAnimated(true)
+  }
+
+  useEffect(() => {
+    // Start animation when the component is in view
+    if (inView && !hasAnimated) {
+      controls.start("visible")
+    }
+  }, [inView, controls])
+
+  const works = [
+    {
+      client: "Summarist",
+      year: "2025",
+      img: work1,
+      title: "Summarist Audio Book App",
+      detail:
+      "",
+    },
+    {
+      client: "Skinstric",
+      year: "2025",
+      img: work2,
+      title: "AI powered skincare",
+      detail: "",
+    },
+    {
+      client: "Library",
+      year: "2024",
+      img: work3,
+      title: "Library e-commerce App",
+      detail: "",
+    },
+  ]
+
+  const opacityVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
+
+  return (
+    <section ref={ref} className="projects" id="projects">
+      <BackgroundLines />
+      <div className="background--glow"></div>
+
+      <div className="projects--grid">
+        <motion.div initial="hidden" animate={controls} variants={opacityVariant} transition={{ duration: 1, delay: 0.5 }} className="projects--grid--title">
+          <h3 className="theme--text">
+            <ScrambleText shuffle delay={0.5}>
+              03
+            </ScrambleText>{" "}
+            <span className="hash">{"//"}</span>{" "}
+            <ScrambleText shuffle delay={0.5}>
+              Expertise
+            </ScrambleText>
+          </h3>
+        </motion.div>
+
+        <div className="projects--grid--content">
+          <div className="projects--grid--content--heading">
+            <h2>
+              <ParaWriting stagger={0.08} text={"My "} sec={"Works"} />
+            </h2>
+          </div>
+          <div className="projects--grid--content--works">
+            {works.map((item, index) => {
+              return (
+                <WorkCard
+                  item={item}
+                  key={index}
+                  // delay={0.1 * index + 1}
+                  // controls={controls}
+                />
+              )
+            })}
+          </div>
+        </div>
+
+        <motion.div initial="hidden" animate={controls} variants={opacityVariant} transition={{ duration: 1, delay: 1 }} onAnimationComplete={() => handleComplete()} className="projects--grid--detail">
+          <p className="theme--detail">
+            <ScrambleText delay={1}>Discover a curated portfolio of projects where each line of code tells a story of problem-solving, creativity, and technical finesse.</ScrambleText>
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  )
+}
